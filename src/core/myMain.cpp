bad15a898cf83956394751b79c20e9ba714de7d3
Tim <timothee.cuq@gmail.com>
2025-06-04 22:17:17 +0200

    project setup

---
+++ src/core/myMain.cpp
@@ -0,0 +1,12 @@
+#include "myMain.h"
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int myMain() {
+
+  return 0;
+}
---
+++ src/test/unitTests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+
+// trivial test to verify unit test implementation
+TEST(TestGame, alwaysValid) {
+
+  EXPECT_EQ(1, 1);
+}
